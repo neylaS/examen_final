b9d26584daeef600e4bd8caa6d617f1c39b162f8
neyla sanjuan <neyla.sanjuan@archidep.com>
2023-02-07 16:25:13 +0100

    last commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 //import { users } from './data'
-import User from './User.js'
+import User from "./User.js";
 
+("use strict");
 
-"use strict";
-
-
-
-
-console.log("hello")
+console.log("hello");
 
 async function getData() {
-    const baseUrl = " https://randomuser.me/api/?results=20";
-    const data = await fetch(baseUrl);
-    const dataJson = await data.json();
-    console.log(dataJson)
+  const baseUrl = " https://randomuser.me/api/?results=20";
+  const data = await fetch(baseUrl);
+  const dataJson = await data.json();
+  console.log(dataJson);
 }
 
+const personne = new User();
 
-const personne = new User()
+const btnName = document.querySelector("#sort--name");
+const btnAge = document.querySelector("#sort--age");
+
+/*btnName.addEventListener("clicl", (e)=>{
+btnName.style.addClassList.backgroundColore = “#06d6a0"
+})*/
